dd7b609bea58e7504afbfc29cff400903f9717c1
tim <tjhickey@brandeis.edu>
2017-09-23 09:26:34 -0400

    updated profiles

--- pages/peereval/client/showProfile.js
+++ pages/peereval/client/showProfile.js
@@ -1,6 +1,22 @@
 Template.showProfile.helpers({
   profile:function(){
-    return Profiles.findOne({id:Meteor.userId()})
+    var profile = Profiles.findOne({id:Meteor.userId()})
+    var u =  Meteor.users.findOne({_id:Meteor.userId()})
+    // this is backward compatability code to add accountType to old existing accounts
+    if (!profile.accountType) {
+      if (u.services && u.services.google){
+        profile.rname = u.services.google.name;
+        profile.remail = u.services.google.email;
+        profile.accountType = "google"
+      }
+      else if (u.emails) {
+        profile.remail = u.emails[0].address;
+        profile.rname =  profile.remail.split("@")[0];
+        profile.accountType="password"
+      }
+      Profiles.update(profile._id,profile);
+    }
+    return profile;
   },
 
 })
@@ -11,6 +27,17 @@ Template.showProfile.events({
     email= instance.$(".js-email").val();
     id = Meteor.userId();
     profile = {name:name,email:email,id:id}
+    var u =  Meteor.users.findOne({_id:Meteor.userId()})
+    if (u.services && u.services.google){
+      profile.rname = u.services.google.name;
+      profile.remail = u.services.google.email;
+      profile.accountType = "google"
+    }
+    else if (u.emails) {
+      profile.remail = u.emails[0].address;
+      profile.rname =  profile.remail.split("@")[0];
+      profile.accountType="password"
+    }
     Profiles.insert(profile);
   },
 
