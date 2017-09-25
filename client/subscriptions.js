2b336d5318e150607eeb895adbe1e098329d4eb4
tim <tjhickey@brandeis.edu>
2017-09-25 11:24:52 -0400

    updated subscriptions

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,10 +1,4 @@
 Meteor.subscribe("theProfiles");
-Meteor.subscribe("userData");
-Meteor.subscribe("theColors");
-Meteor.subscribe("theIPs");
-Meteor.subscribe("theConnections");
 Meteor.subscribe("theQuestions");
 Meteor.subscribe("theAnswers");
 Meteor.subscribe("theReviews");
-
-Meteor.subscribe("theChats");
