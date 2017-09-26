f84ac25699aef00d3b30dc5081a6b0423191bd71
tim <tjhickey@brandeis.edu>
2017-09-25 20:15:35 -0400

    revised sort order on summary

--- pages/peereval/client/questionsummary.js
+++ pages/peereval/client/questionsummary.js
@@ -1,12 +1,12 @@
 Template.questionsummary.helpers({
   answers:function(qid){
-    var as = Answers.find({question:qid});
+    var as = Answers.find({question:qid},{sort:{answer:1}, reactive:false});
     console.dir(as.fetch())
     return as;
   },
 
   reviews:function(aid){
-    var rs = Reviews.find({answer_id:aid});
+    var rs = Reviews.find({answer_id:aid},{sort:{rating:1}, reactive:false});
     console.dir(rs.fetch())
     return rs;
   }
