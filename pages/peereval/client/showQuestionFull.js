a200a79e0e696ab998d6c50f348cc9e7597ca396
tim <tjhickey@brandeis.edu>
2017-10-06 05:28:31 -0400

    added a save button to UI

--- pages/peereval/client/showQuestionFull.js
+++ pages/peereval/client/showQuestionFull.js
@@ -9,24 +9,55 @@ myAnswerId should only be undefined if they haven't tried to solve
 this problem yet!!
 */
 
+Template.showQuestionFull.onCreated(function(){
 
+    this.state =  new ReactiveDict();
+    this.state.setDefault({
+      submitMode:false,
+    });
+
+})
 
 Template.showQuestionFull.helpers({
+
+  submitted: function(){
+    console.log('in submitted')
+    console.dir(this);
+    return(this.submitted)
+  },
 
   answer: function(){
     var ans= Answers.findOne(
       {createdBy:Meteor.userId(),
        question:this._id});
+    if (ans && ans.submitted==undefined) {
+      ans.submitted = true;
+      Answers.update(ans._id,{$set:{submitted:true}});
+    }
 
     return ans; // this could be undefined!
   },
+
+  submitMode: function(){
+    var instance = Template.instance();
+    return instance.state.get('submitMode')
+  }
 
 })
 
 
 Template.showQuestionFull.events({
+  "change .js-submit-state"(event, instance){
+
+    instance.state.set('submitMode',event.target.checked);
+
+  },
+
   "click .js-submit-answer": function(event){
     var instance = Template.instance();
+    var submitMode = instance.state.get('submitMode');
+
+
     event.preventDefault();
     var answer = $(".js-student-answer").val();
 
@@ -35,18 +66,37 @@ Template.showQuestionFull.events({
      * this user. This will allow us to make sure the user
      * always gets new answers to review, if there are any..
     */
+
+    // first we see if the user has already saved an answer
+    var lastAnswer =
+       Answers.findOne({question:this._id,
+                     createdBy:Meteor.userId()})
+
+    // then we construct the current answer
     var answerData =
       { answer:answer,
         question:this._id,
         createdAt:new Date(),
         createdBy: Meteor.userId(),
         userName: 'anon',
+        submitted: false,
         myReviews:[],  // list of _id's of students reviewed by this person
         myReviewers:[],
         class:this.class_id,
       };
 
-    Answers.insert(answerData);
+    answerData.submitted = submitMode;
+    console.dir([lastAnswer,answerData]);
+
+    // now we either update the saved version or insert the new version
+    if (lastAnswer){
+      Answers.update(lastAnswer._id,answerData)
+    } else {
+      Answers.insert(answerData)
+    }
+
+    var zz = Answers.findOne(answerData);
+    console.dir(zz);
 
   }
 })
