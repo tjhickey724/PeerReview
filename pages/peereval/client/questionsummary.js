97aa9e1682359c56cd9894f158d61236e9289032
tim <tjhickey@brandeis.edu>
2017-10-08 15:17:09 -0400

    fixed summary bug

--- pages/peereval/client/questionsummary.js
+++ pages/peereval/client/questionsummary.js
@@ -7,6 +7,12 @@ Template.questionsummary.helpers({
   reviews:function(aid){
     var rs = Reviews.find({answer_id:aid},{sort:{rating:1}});
     return rs;
+  },
+
+  hasAnswered:function(qid){
+    // return true when the user has answered this question.
+    var answer = Answers.find({question:qid,createdBy:Meteor.userId()});
+    return answer;
   }
 
 })
